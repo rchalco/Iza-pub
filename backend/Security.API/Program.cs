694d1187b732e5e394dd01ce005806bdedc127e3
mikyches <68037447+mikyches@users.noreply.github.com>
2024-07-11 23:11:56 -0400

    cambios

--- backend/Security.API/Program.cs
+++ backend/Security.API/Program.cs
@@ -1,44 +1,46 @@
+using PlumbingProps.Config;
+using System.Net;
+
+
+///Configuration host
+var MyAllowSpecificOrigins = "MyPolicy";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin();
+                      });
+});
 
+if (ConfigManager.GetConfiguration().GetSection("mode").Value.Equals("server"))
+{
+    IPAddress iPAddress = IPAddress.Parse(ConfigManager.GetConfiguration().GetSection("iplookup").Value!);
+    int portServer = Convert.ToInt32(ConfigManager.GetConfiguration().GetSection("port").Value!);
+    builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+    {
+        serverOptions.Listen(iPAddress, portServer);
+    });
+}
+
+builder.Services.AddControllers();
+
+///Configuration application
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseCors(builder =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
