afc1800fe73a51746635a697a8add154cf8ee794
mikyches <68037447+mikyches@users.noreply.github.com>
2026-05-13 20:51:56 -0400

    cambios enviroment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -87,18 +87,6 @@ export const LogoVoucher = '';
 // export const URL_CARDS = 'http://localhost:8001/api/Tarjeta/';
 // export const URL_FINGERS = 'http://localhost:8001/api/Biometric/';
 
-//SERVER PRUEBA
-
-// export const URL_MIROVENTAOPERACION = 'http://localhost:8001/api/MicroventaOperacion/';
-// export const URL_TINTORERIA = 'http://localhost:8001/api/Tintoreria/';
-// export const URL_SECURITY = 'http://localhost:5294/api/APISeguridad/';
-// export const URL_INVENTARIO = 'http://localhost:5294/api/APIIventario/';
-// export const URL_MIROVENTA = 'http://localhost:5294/api/APIVenta/';
-// //export const URL_SECURITY = 'http://localhost:5294/api/APISeguridad/';
-// export const URL_PERSON = 'http://localhost:8001/api/Person/';
-// export const URL_CARDS = 'http://localhost:8001/api/Tarjeta/';
-// export const URL_FINGERS = 'http://localhost:8001/api/Biometric/';
-
 //SERVER PROD BARQUITO
 // export const URL_MIROVENTAOPERACION ='http://localhost:8001/api/MicroventaOperacion/';
 // export const URL_TINTORERIA = 'http://localhost:8001/api/Tintoreria/';
@@ -110,26 +98,26 @@ export const LogoVoucher = '';
 // export const URL_FINGERS = 'http://localhost:8001/api/Biometric/';
 
 //SERVER PROD MANTRA
-export const URL_MIROVENTAOPERACION ='http://localhost:8001/api/MicroventaOperacion/';
-export const URL_TINTORERIA = 'http://localhost:8001/api/Tintoreria/';
-export const URL_SECURITY = 'http://155.138.212.216:8034/api/APISeguridad/';
-export const URL_INVENTARIO = 'http://155.138.212.216:8034/api/APIIventario/';
-export const URL_MIROVENTA = 'http://155.138.212.216:8034/api/APIVenta/';
-export const URL_PERSON = 'http://localhost:8001/api/Person/';
-export const URL_CARDS = 'http://localhost:8001/api/Tarjeta/';
-export const URL_FINGERS = 'http://localhost:8001/api/Biometric/';
-
-//SERVER PRUEBA
-
-// export const URL_MIROVENTAOPERACION = 'http://localhost:8001/api/MicroventaOperacion/';
+// export const URL_MIROVENTAOPERACION ='http://localhost:8001/api/MicroventaOperacion/';
 // export const URL_TINTORERIA = 'http://localhost:8001/api/Tintoreria/';
-// export const URL_SECURITY = 'http://localhost:5294/api/APISeguridad/';
-// export const URL_INVENTARIO = 'http://localhost:5294/api/APIIventario/';
-// export const URL_MIROVENTA = 'http://localhost:5294/api/APIVenta/';
-// //export const URL_SECURITY = 'http://localhost:5294/api/APISeguridad/';
+// export const URL_SECURITY = 'http://155.138.212.216:8034/api/APISeguridad/';
+// export const URL_INVENTARIO = 'http://155.138.212.216:8034/api/APIIventario/';
+// export const URL_MIROVENTA = 'http://155.138.212.216:8034/api/APIVenta/';
 // export const URL_PERSON = 'http://localhost:8001/api/Person/';
 // export const URL_CARDS = 'http://localhost:8001/api/Tarjeta/';
 // export const URL_FINGERS = 'http://localhost:8001/api/Biometric/';
+
+//SERVER PRUEBA
+
+export const URL_MIROVENTAOPERACION = 'http://localhost:8001/api/MicroventaOperacion/';
+export const URL_TINTORERIA = 'http://localhost:8001/api/Tintoreria/';
+export const URL_SECURITY = 'http://localhost:5294/api/APISeguridad/';
+export const URL_INVENTARIO = 'http://localhost:5294/api/APIIventario/';
+export const URL_MIROVENTA = 'http://localhost:5294/api/APIVenta/';
+//export const URL_SECURITY = 'http://localhost:5294/api/APISeguridad/';
+export const URL_PERSON = 'http://localhost:8001/api/Person/';
+export const URL_CARDS = 'http://localhost:8001/api/Tarjeta/';
+export const URL_FINGERS = 'http://localhost:8001/api/Biometric/';
 
 // /*
 //  * For easier debugging in development mode, you can import the following file
